c7b01d78d304882a7bdbdfca4e61168b58550b3f
Reward Khe <rewardkhe@outlook.com>
2023-01-12 23:31:53 +1100

    startoff file

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        //get input y/n from user if y, output membersfile else cancel.
+    }
+}
